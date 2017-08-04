c2242c5bdb60eef0062369875b0d4400c940c49d
Richard Harper <dick.harper@gmail.com>
2017-08-04 07:36:29 -0600

    Initial commit

---
+++ Javelin.Repositories/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Javelin.Repositories
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton Instance = new Singleton();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            return Instance;
+        }
+
+        public long GetTemp()
+        {
+            return 75;
+        }
+    }
+}
---
+++ Javelin.Services.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Javelin.Services.Web.MvcApplication" Language="C#" %>
